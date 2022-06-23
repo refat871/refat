035f95ec3b38c233704aa3020c8a7083102ee15f
refat871 <57785695+refat871@users.noreply.github.com>
2022-06-23 21:36:41 +0600

    Basic things about HTML.

---
+++ Lab_task_1/webtech lab task_1.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h2>Donor Information</h2>
+<form>
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname" ><br>
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname" ><br>
+   <label for="fname">Company:</label>
+  <input type="text" id="fname" name="fname" ><br>
+  <label for="lname">Address1:</label>
+  <input type="text" id="lname" name="lname" ><br>
+   <label for="fname">Address2:</label>
+  <input type="text" id="fname" name="fname" ><br>
+  <label for="lname">City:</label>
+  <input type="text" id="lname" name="lname" ><br>
+  
+  <label for="State">Select a state:</label>
+  <select name="State" id="Stae">
+    <option value="Cumilla">Cumilla</option>
+    <option value="Dhaka">Dhaka</option>
+    <option value="Khulna">Khulna</option>
+   
+  </select><br>
+  
+  <label for="lname">Zip Code:</label>
+  <input type="text" id="lname" name="lname" ><br>
+
+  <label for="Country">Select a Country:</label>
+  <select name="Country" id="Country">
+    <option value="Bangladesh">Bangladesh</option>
+    <option value="India"">India</option>
+    <option value="UAS">UAS</option>
+    <option value="Uae">Uae</option>
+  </select><br>
+  
+  <label for="lname">phone:</label>
+  <input type="text" id="lname" name="lname" ><br>
+   <label for="fname">Fax:</label>
+  <input type="text" id="fname" name="fname" ><br>
+  <label for="lname">Email:</label>
+  <input type="text" id="lname" name="lname" ><br>
+  
+  <p>Donation Amount:</p>
+  <input type="radio" id="None" name="Amount" value="None">
+� <label for="None">None</label><br>
+� <input type="radio" id="$50" name="Amount" value="$50">
+� <label for="$50">$50</label><br>
+� <input type="radio" id="$100" name="Amount" value="$100">
+� <label for="$100">$100</label>
+
+<p>Recuring Donation</p>
+
+<form action="/action_page.php">
+  <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+  <label for="vehicle1"> I am interested in giving on a regular basis</label><br>
+  
+  <h2>Honorarium and Memorial Donation Information</h2>
+  <p>I  would like to make this donation:</p>
+  <input type="radio" id="None" name="Amount" value="None">
+� <label for="None">To honor</label><br>
+� <input type="radio" id="$50" name="Amount" value="$50">
+� <label for="$50">In memory of</label><br>
+ <label for="fname">Name:</label>
+  <input type="text" id="fname" name="fname" ><br>
+   <label for="fname">Acknowledge Donation to:</label>
+  <input type="text" id="fname" name="fname" ><br>
+   <label for="fname">Address:</label>
+  <input type="text" id="fname" name="fname" ><br>
+   <label for="fname">City:</label>
+  <input type="text" id="fname" name="fname" ><br>
+  
+   <label for="State">Select a state:</label>
+  <select name="State" id="Stae">
+    <option value="Cumilla">Cumilla</option>
+    <option value="Dhaka">Dhaka</option>
+    <option value="Khulna">Khulna</option>
+	 </select><br>
+  <label for="fname">Zip Code</label>
+  <input type="text" id="fname" name="fname" ><br>
+
+
+   
+  
+</form>
+
+<p>This is a paragraph.</p>
+
+</body>
+</html>
